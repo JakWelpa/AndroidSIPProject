c6d275e9a4deba2fb1b00244e8b33f7dbf426b04
Łukasz <lux9@o2.pl>
2016-11-15 20:03:50 +0100

    Commit test

--- app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/MainActivity.java
+++ app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/MainActivity.java
@@ -4,10 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+/* Test */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void test(){
+        int i = 0;
     }
 }
