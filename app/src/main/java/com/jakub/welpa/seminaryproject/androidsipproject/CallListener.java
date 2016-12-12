fab4efe0bc9f9ec808b240771636f794b8ad318a
JakWelpa <jakwelpa@gmail.com>
2016-12-12 21:51:06 +0100

    Bugfix with callListener

--- app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/CallListener.java
+++ app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/CallListener.java
@@ -7,38 +7,38 @@ import android.content.Intent;
 import android.net.sip.SipAudioCall;
 import android.net.sip.SipProfile;
 
-public class CallListener {
-//
-//    @Override
-//    public void onReceive(Context context, Intent intent) {
-//        SipAudioCall calling = null;
-//        try {
-//            SipAudioCall.Listener listener = new SipAudioCall.Listener() {
-//                @Override
-//                public void onRinging(SipAudioCall call, SipProfile caller) {
-//                    try {
-//                        call.answerCall(30);
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            };
-//
-//            MainActivity main = (MainActivity) context;
-//
-//            calling = main.manager.takeAudioCall(intent, listener);
-//            calling.answerCall(30);
-//            calling.startAudio();
-//            calling.setSpeakerMode(true);
-//
-//            main.updateCallStatus(calling);
-//
-//        } catch (Exception e) {
-//            if (calling != null) {
-//                calling.close();
-//            }
-//        }
-//
-//
-//    }
+public class CallListener extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SipAudioCall calling = null;
+        try {
+            SipAudioCall.Listener listener = new SipAudioCall.Listener() {
+                @Override
+                public void onRinging(SipAudioCall call, SipProfile caller) {
+                    try {
+                        call.answerCall(30);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+            MainActivity main = (MainActivity) context;
+
+            calling = main.manager.takeAudioCall(intent, listener);
+            calling.answerCall(30);
+            calling.startAudio();
+            calling.setSpeakerMode(true);
+
+            main.updateCallStatus(calling);
+
+        } catch (Exception e) {
+            if (calling != null) {
+                calling.close();
+            }
+        }
+
+
+    }
 }
