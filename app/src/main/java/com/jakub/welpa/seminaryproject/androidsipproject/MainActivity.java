edc794a98040cfeec6d0d5a03f6daed91b58e0ca
JakWelpa <jakwelpa@gmail.com>
2016-12-12 19:29:26 +0100

    Add listener for calls

--- app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/MainActivity.java
+++ app/src/main/java/com/jakub/welpa/seminaryproject/androidsipproject/MainActivity.java
@@ -2,20 +2,21 @@ package com.jakub.welpa.seminaryproject.androidsipproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-    private void test(){
-        int i = 0;
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
     }
-    private void test2(){
-        int i = 2;
-    }
-
 }
